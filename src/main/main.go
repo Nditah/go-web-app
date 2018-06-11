cb52e55c10541eac66353a128477f2324874ccda
Nditah <nditah@gmail.com>
2018-06-11 08:09:27 +0100

    Feature : Add Book to DataBase from Search Result
    The user can click a book to be added to mysql database.

--- src/main/main.go
+++ src/main/main.go
@@ -65,6 +65,33 @@ func main() {
 		
 	})
 
+	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
+		var book ClassifyBookResponse
+		var err error
+
+		if book, err = find(r.FormValue("id")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		if err = db.Ping(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		
+		// Prepare statement for inserting data
+		stmtIns, err := db.Prepare("INSERT INTO books (pk, title, author, id, classification) VALUES (?, ?, ?, ?, ?)") // ? = placeholder
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
+
+		_, err = stmtIns.Exec(nil, book.BookData.Title, book.BookData.Author, book.BookData.ID,
+				book.Classification.MostPopular)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	})
+
 	fmt.Println(http.ListenAndServe(":7000", nil))
 }
 
@@ -72,25 +99,55 @@ type ClassifySearchResponse struct {
 	Results []SearchResult `xml:"works>work"`
 }
 
+type ClassifyBookResponse struct {
+	BookData struct {
+		Title string `xml:"title,attr"`
+		Author string `xml:"author,attr"`
+		Year string `xml:"hyr,attr"`
+		ID string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recommendations>ddc>mostPopular"`
+}
+
 func search(query string) ([]SearchResult, error) {
+	var c ClassifySearchResponse
+	link := "http://classify.oclc.org/classify2/Classify?&summary=true&title="
+	body, err := classifyAPI(link + url.QueryEscape(query))
+
+	fmt.Println(link + url.QueryEscape(query))
+	
+	if err != nil {
+		return []SearchResult{}, err
+	}
+
+	err = xml.Unmarshal(body, &c)
+	return c.Results, err
+}
+
+func find(id string) (ClassifyBookResponse, error) {
+	var c ClassifyBookResponse
+	link := "http://classify.oclc.org/classify2/Classify?&summary=true&owi="+ url.QueryEscape(id)
+	body, err := classifyAPI(link)
+
+	if err != nil {
+		return ClassifyBookResponse{}, err 
+	}
+
+	err = xml.Unmarshal(body, &c)
+	return c, err
+}
+
+func classifyAPI(url string) ([]byte, error) {
 	var resp *http.Response
 	var err error
-	var link string = "http://classify.oclc.org/classify2/Classify?&summary=true&title="
-	var input string = link + url.QueryEscape(query)
 
-	//fmt.Println(input)
-
-	if resp, err = http.Get(input); err != nil {
-		return []SearchResult{}, err
+	if resp, err = http.Get(url); err != nil {
+		return []byte{}, err
 	}
 
 	defer resp.Body.Close()
-	var body []byte
-	if body, err = ioutil.ReadAll(resp.Body); err != nil {
-		return []SearchResult{}, err
-	}
 
-	var c ClassifySearchResponse
-	err = xml.Unmarshal(body, &c)
-	return c.Results, err
+	return ioutil.ReadAll(resp.Body)
 }
